e402ebb78c21212f9063fe1c05b4e87cab607bf0
Xeuz27 <jesusg.1301.jg@gmail.com>
2024-03-13 17:04:52 -0400

    chore: added site config not in use yet

---
+++ src/data/site.config.ts
@@ -0,0 +1,13 @@
+interface SiteConfig {
+  author: string;
+  title: string;
+  description: string;
+}
+
+export const siteConfig: SiteConfig = {
+  author: "yidev27", // Site author
+  title:
+    "Jesus gutierrez • Desarrollador y Programador Web FullStack • Curriculum", // Site title.
+  description:
+    "Contrata a yidev27 para crear tu Paginas y aplicaciones web. Comprometido en impulsar el avance tecnológico, mi enfoque está en liderar a emprendedores y pequeños empresarios hacia nuevas perspectivas de éxito en el ámbito digital. Brindando conocimientos y estrategias para destacar y prosperar en el mundo tecnológico.", // Description to display in the meta tags
+};
