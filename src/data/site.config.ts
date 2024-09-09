8be61e56948b8ad39c6c44d83a3f2932feabc872
Xeuz27 <jesusg.1301.jg@gmail.com>
2024-09-08 22:59:08 -0400

    chore: two more values to the data

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -2,6 +2,8 @@ interface SiteConfig {
   author: string;
   title: string;
   description: string;
+  mision: string;
+  vision: string;
 }
 
 export const siteConfig: SiteConfig = {
@@ -10,4 +12,9 @@ export const siteConfig: SiteConfig = {
     "Jesus gutierrez • Desarrollador y Programador Web FullStack • Curriculum", // Site title.
   description:
     "Contrata a yidev27 para crear tu Paginas y aplicaciones web. Comprometido en impulsar el avance tecnológico, mi enfoque está en liderar a emprendedores y pequeños empresarios hacia nuevas perspectivas de éxito en el ámbito digital. Brindando conocimientos y estrategias para destacar y prosperar en el mundo tecnológico.", // Description to display in the meta tags
+  mision:
+    "Impulsar el avance tecnológico al liderar a emprendedores y pequeños empresarios hacia nuevas perspectivas de éxito en el ámbito digital. Proporcionar conocimientos y estrategias innovadoras para destacar y prosperar en un mundo tecnológico en constante evolución.",
+
+  vision:
+    "Convertirme en un referente reconocido por mi capacidad para guiar a emprendedores y empresarios hacia el éxito digital, inspirando cambios significativos y contribuyendo al crecimiento sostenible de la industria tecnológica a nivel global",
 };
