47f9377508432a1177c445a2b0569faa50d2f877
Xeuz27 <jesusg.1301.jg@gmail.com>
2024-12-04 09:03:24 -0400

    starting point to extend colors and classes into a theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,21 @@ export default {
       gridTemplateColumns: {
         responsive: "repeat(auto-fit, minmax(200px, 1fr))",
       },
+      colors: {
+        separatorPrint:
+          "#e7e5e480" /* equivalent to stone-200/50 -> horizontal separator for items in flex display PRINT: */,
+        separator:
+          "#44403c99" /* equivalent to stone-700/60 -> horizontal separator for items in flex display */,
+      },
+      backgroundColor: {
+        main: "#010411",
+      },
+      textColor: {
+        mainTitle: "#fafaf9" /* equivalent to stone-50 -> h1 */,
+        subtitle: "#e7e5e4cc" /* equivalent to stone-200/80 -> h2 */,
+        sectionTitle: "#fafaf9e6" /* equivalent to stone-50/90 -> h3 */,
+        inherit: "inherit",
+      },
     },
   },
   plugins: [],
