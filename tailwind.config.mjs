4452b557d2db555ff223e30078d7dcc48d9564d9
Xeuz27 <jesusg.1301.jg@gmail.com>
2024-12-05 14:18:19 -0400

    style: extending screens, changing bg color for main, extending font sizes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      screens: {
+        xs: "475px",
+      },
       gridTemplateColumns: {
         responsive: "repeat(auto-fit, minmax(200px, 1fr))",
       },
@@ -13,13 +16,16 @@ export default {
           "#44403c99" /* equivalent to stone-700/60 -> horizontal separator for items in flex display */,
       },
       backgroundColor: {
-        main: "#010411",
+        main: "#050c23",
       },
       textColor: {
         mainTitle: "#fafaf9" /* equivalent to stone-50 -> h1 */,
         subtitle: "#e7e5e4cc" /* equivalent to stone-200/80 -> h2 */,
         sectionTitle: "#fafaf9e6" /* equivalent to stone-50/90 -> h3 */,
         inherit: "inherit",
+      },
+      fontSize: {
+        sizeInherit: "inherit",
       },
     },
   },
