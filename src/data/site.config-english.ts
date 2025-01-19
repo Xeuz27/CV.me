48c45e33cb8bfe36a7339c2d94c0f388b66dade6
Xeuz27 <jesusg.1301.jg@gmail.com>
2025-01-18 21:38:35 -0400

    feat: cloning site data to add english versions too

--- src/data/site.config-english.ts
+++ src/data/site.config-english.ts
@@ -8,11 +8,12 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
   author: "yidev27", // Site author
-  title: "Jesus gutierrez • Desarrollador Web FullStack • Curriculum", // Site title.
+  title: "Jesus gutierrez • Fullstack Web developer • Venezuela", // Site title.
   description:
-    "Contrata a yidev27 para crear tus Paginas y aplicaciones web. Comprometido en impulsar el avance tecnológico, Brindando conocimientos y estrategias para destacar y prosperar en el mundo tecnológico.", // Description to display in the meta tags
+    "Hire yidev27 To create your Website. App and Websites Developer. Committed to boost technological advancement, my focus is on leading entrepreneurs and small business owners towards new perspectives of success in the digital field. Providing knowledge and strategies to stand out and thrive in the technological world.", // Description to display in the meta tags
   mision:
     "Impulsar el avance tecnológico al liderar a emprendedores y pequeños empresarios hacia nuevas perspectivas de éxito en el ámbito digital. Proporcionar conocimientos y estrategias innovadoras para destacar y prosperar en un mundo tecnológico en constante evolución.",
+
   vision:
     "Convertirme en un referente reconocido por mi capacidad para guiar a emprendedores y empresarios hacia el éxito digital, inspirando cambios significativos y contribuyendo al crecimiento sostenible de la industria tecnológica a nivel global",
 };
