4209614bc99826c0eeddcabaf7a6ce753d22c954
Xeuz27 <jesusg.1301.jg@gmail.com>
2025-05-08 13:42:48 -0400

    sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://yidev.vercel.app",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      lastmod: new Date(),
+    }),
+  ],
   i18n: {
     defaultLocale: "es",
     locales: ["en", "es"],
