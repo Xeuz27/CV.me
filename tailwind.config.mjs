e575fac47186882fe59c819a79c17997d0efbe44
Jesus Gutierrez - Xeuz27 <jesusg.1301.jg@gmail.com>
2025-05-29 12:28:33 -0400

    style: ensuring la misma escala de grises para las tipografias (slate or gray )

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,15 +13,15 @@ export default {
         separatorPrint:
           "#e7e5e480" /* equivalent to stone-200/50 -> horizontal separator for items in flex display PRINT: */,
         separator:
-          "#44403c99" /* equivalent to stone-700/60 -> horizontal separator for items in flex display */,
+          "#37415199" /* equivalent to gray-700/60 -> horizontal separator for items in flex display */,
       },
       backgroundColor: {
         main: "#050c23",
       },
       textColor: {
-        mainTitle: "#fafaf9" /* equivalent to stone-50 -> h1 */,
-        subtitle: "#e7e5e4cc" /* equivalent to stone-200/80 -> h2 */,
-        sectionTitle: "#fafaf9e6" /* equivalent to stone-50/90 -> h3 */,
+        mainTitle: "#f9fafb" /* equivalent to gray-50 -> h1 */,
+        subtitle: "#e5e7ebcc" /* equivalent to gray-200/80 -> h2 */,
+        sectionTitle: "#f9fafbe6" /* equivalent to gray-50/90 -> h3 */,
         inherit: "inherit",
       },
       fontSize: {
